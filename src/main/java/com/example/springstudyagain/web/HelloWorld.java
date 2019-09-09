31720b475162ed44f8785b4290d9f11569e27f76
caohong <568034700@qq.com>
2019-09-09 19:07:47 +0800

    AA

--- src/main/java/com/example/springstudyagain/web/HelloWorld.java
+++ src/main/java/com/example/springstudyagain/web/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.springstudyagain.web;
 
 import com.example.springstudyagain.repository.ConfigurationAnnotationEntityTest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,14 @@ public class HelloWorld {
     @RequestMapping("/")
     public String home() {
         return "hello world";
+    }
+
+    @Value("${person.last-name}")
+    String name;
+
+    @RequestMapping("/a")
+    public String hello() {
+        return "hello" + name;
     }
 
 
--- src/main/test/java/test.java
+++ src/main/test/java/test.java
@@ -1,8 +1,11 @@
 import com.example.springstudyagain.SpringStudyAgainApplication;
 import com.example.springstudyagain.model.ConfigurationBean;
+import com.example.springstudyagain.model.ConfigurationPropertiesBean;
 import com.example.springstudyagain.repository.ConfigurationAnnotationEntityTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,10 +17,18 @@ public class test {
     @Resource
     ConfigurationBean configurationBean;
 
+    @Autowired
+    ConfigurationPropertiesBean configurationPropertiesBean;
+
     @Test
     public void ConfigurationAnnotationTest() {
         ConfigurationAnnotationEntityTest configurationAnnotationEntityTest = configurationBean.configurationAnnotationTest();
         configurationAnnotationEntityTest.go();
+    }
+
+    @Test
+    public void ConfigurationPropertiesAnnotationTest() {
+        System.out.println(configurationPropertiesBean);
     }
 
 }

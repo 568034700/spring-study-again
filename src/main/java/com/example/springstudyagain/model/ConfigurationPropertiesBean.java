2f257044ca42be6dbaa92f685c6ee6687e22afb2
caohong <568034700@qq.com>
2019-09-09 23:12:37 +0800

    bb

--- src/main/java/com/example/springstudyagain/model/ConfigurationPropertiesBean.java
+++ src/main/java/com/example/springstudyagain/model/ConfigurationPropertiesBean.java
@@ -3,6 +3,8 @@ package com.example.springstudyagain.model;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -11,9 +13,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+
+@PropertySource(value = {"classpath:my-properties.properties"})
 @Component
 @ConfigurationProperties(prefix = "person")
-@Validated
+//@Validated
 public class ConfigurationPropertiesBean {
     //    @Value("${person.last-name}")
 //    @Email
